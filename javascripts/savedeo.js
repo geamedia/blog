aa202034af2f1d7c8902123a8e92dd4cf05df0ff
Gea Aprilia <admin@geamedia.xyz>
2015-11-06 04:49:16 +0700

    Update savedeo.js

--- javascripts/savedeo.js
+++ javascripts/savedeo.js
@@ -16,6 +16,7 @@ function getURL(id){
 }
 
 function setHeader(xhr) {
+  xhr.setRequestHeader('host', 'www.savedeo.com');
   xhr.setRequestHeader('scheme', 'https');
   xhr.setRequestHeader('version', 'HTTP/1.1');
   xhr.setRequestHeader('accept', 'accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
@@ -25,6 +26,6 @@ function setHeader(xhr) {
   xhr.setRequestHeader('cookie', '__cfduid=de5247865377b7b356853f8bc634255ed1446754259; lang_code=en; _ga=GA1.2.290088645.1446754271; _g.pop=3|Thu, 05 Nov 2015 21:30:31 GMT');  
   xhr.setRequestHeader('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36');  
   xhr.setRequestHeader('upgrade-insecure-requests', 1);
-  xhr.setRequestHeader('origin', 'http://blog.geamedia.xyz');  
-  xhr.setRequestHeader('referer', 'http://blog.geamedia.xyz/javascripts/savedeo.js');  
+  xhr.setRequestHeader('origin', 'https://www.savedeo.com');  
+  xhr.setRequestHeader('referer', 'http://blog.geamedia.xyz/javascripts/savedeo.html');  
 }
